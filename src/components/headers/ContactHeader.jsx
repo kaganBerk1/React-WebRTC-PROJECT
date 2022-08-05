c2c6f7eaa428ac12b3bd77d313e9c63b0ec607d5
Elbis1 <64051166+Elbis1@users.noreply.github.com>
2022-08-06 01:35:29 +0300

    contact page header style done

--- src/components/headers/ContactHeader.jsx
+++ src/components/headers/ContactHeader.jsx
@@ -3,14 +3,23 @@ import { Avatar,Tooltip } from 'flowbite-react';
 import video from "../../images/video.svg"
 import phone from "../../images/phone.svg"
 import back from "../../images/back.svg"
+import { useNavigate } from 'react-router-dom';
 
 export default function ContactHeader() {
+    const navigateTo = useNavigate();
+    function goBack(){
+        navigateTo(-1)
+    }
+
   return (
-    <div className="sticky flex justify-center mt-5">
+    <div className="sticky flex justify-center mt-6">
         <div  className=' flex justify-between shadow-lg rounded-xl  w-5/6 h-14 text-[#313131] bg-white w-6/6'>   
-            <div className="flex space-y-1 font-medium dark:text-white">
-                <img  className='ml-2 mr-6 w-6 rotate-180' src={back} alt="back" />
-                <div className='flex flex-col'>
+            <div className="flex space-y-1 font-medium dark:text-white items-center">
+                <Tooltip content={"Back"} style="dark"  animation="duration-500">
+                    <img style={{width:"40px",paddingLeft:"6px",paddingRight:"6px"}}  className='ml-3 pt-2 pb-2  rounded-full  mt-4 mb-4  w-6 hover:bg-[#f1f1f1] hover:duration-500 duration-500 rotate-180 cursor-pointer' onClick={goBack} src={back} alt="back" />
+
+                </Tooltip>
+                <div className='flex ml-4 flex-col'>
                     <div>
                     Jese Leos
                     </div>
@@ -19,9 +28,13 @@ export default function ContactHeader() {
                     </div>
                 </div>
             </div>
-            <div className='flex mr-5'>
-                <img className='w-7 mr-6' src={video} alt="video" />
-                <img className='w-7' src={phone} alt="phone" />
+            <div className='flex mr-5 items-center'>
+                <Tooltip content={"Video Call"} style="dark"  animation="duration-500">
+                    <img className=' cursor-pointer w-7 mr-6' src={video} alt="video" />
+                </Tooltip>
+                <Tooltip content={"Phone Call"} style="dark"  animation="duration-500">
+                    <img className='w-7 cursor-pointer' src={phone} alt="phone" />
+                </Tooltip>
 
             </div>
       </div>
