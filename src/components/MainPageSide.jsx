b9df954489c81a319330ef271422a324a0211aa6
Elbis1 <64051166+Elbis1@users.noreply.github.com>
2022-08-06 00:28:24 +0300

    chec icon and styles added

--- src/components/MainPageSide.jsx
+++ src/components/MainPageSide.jsx
@@ -2,6 +2,7 @@ import 'flowbite';
 import React, { useRef, useState } from 'react'
 import profileImage from "./../images/butters.png"
 import edit from "./../images/edit.svg"
+import check from "./../images/check-1.svg"
 import copyIcon from "./../images/copy-1.svg"
 import { Avatar, Tooltip,Textarea } from 'flowbite-react';
 import useClickOutside from '../helper/useClickOutside ';
@@ -10,14 +11,16 @@ export default function MainPageSide() {
     const [name,setName]=useState("Kağan Gencer")
     const [about,setAbout]=useState("Hey there, I am using... asdasdasd asdasdas asdasd")
     const [isCopied, setCopied] = React.useState(false);
-    const [disabledInput,setDisabledInput]=useState(false)
-    const [disabledTextarea,setDisabledTextarea]=useState(false)
+    const [disabledInput,setDisabledInput]=useState(true)
+    const [disabledTextarea,setDisabledTextarea]=useState(true)
 
     const nameInput = useRef(null);
     const aboutInput = useRef(null);
 
     const clickRefInput = React.useRef();
     const clickRefTextarea = React.useRef();
+
+    
 
     useClickOutside(clickRefInput,()=>{
         setDisabledInput(true)
@@ -29,20 +32,29 @@ export default function MainPageSide() {
 
 
     function handleInputDisabled(){
+       if(disabledInput){
         setDisabledInput(false)
         setTimeout(()=>{
             nameInput.current.focus();
           },50)
+       }else{
+        setDisabledInput(true)
+       }
     }
 
 
 
 
     function handleTextArea(){
-        setDisabledTextarea(false)
-        setTimeout(()=>{
-            aboutInput.current.focus();
-        },50)
+        if(disabledTextarea){
+            setDisabledTextarea(false)
+            setTimeout(()=>{
+                aboutInput.current.focus();
+            },50)
+        }else{
+            setDisabledTextarea(true)
+        }
+ 
     }
 
     function copy(){
@@ -69,18 +81,17 @@ export default function MainPageSide() {
                 <span className='text-[#B8336A] text-xl font-semibold cursor-default'>Your name</span>
                 <div  ref={clickRefInput}  className='flex justify-between items-center'>
                     <input   ref={nameInput}  disabled={disabledInput}  className='text-[#f1f1f1] focus:opacity-100 opacity-80 cursor-pointer w-5/6 bg-transparent outline-offset-0  outline-none focus:bg-transparent font-normal focus:text-[#fff] text-xl focus:border-b-2 mt-2 border-b-2 border-transparent focus:border-[#B8336A] ' value= {name}></input>
-                    <Tooltip  content={"Edit"} style="light"  animation="duration-500">
-                        <img   className='w-5 cursor-pointer mb-1 duration-300 hover:rotate-6 ' onClick={handleInputDisabled} src={edit} alt="edit" />
+                    <Tooltip  content={disabledInput?"Edit":"Save"} style="light"  animation="duration-500">
+                        <img   className={`animate-[fadeIn_1s_ease-in-out] w-5 cursor-pointer mb-1 duration-500 hover:rotate-6 `} onClick={handleInputDisabled} src={disabledInput?edit:check} alt="edit" />
                     </Tooltip>
                 </div>
             </div>
             <div className=' mt-4 w-5/6 pr-2 pl-2 justify-between  '>
                 <span className='text-[#B8336A]  text-xl font-semibold cursor-default'>About</span>
                 <div ref={clickRefTextarea} className='flex justify-between items-center'>
-{/*                     <Textarea ref={aboutInput}  id="about"  rows={4}  className='focus:text-[#fff] focus:opacity-100 opacity-80 text-[#f1f1f1] mt-2 cursor-pointer w-5/6 bg-transparent outline-offset-0 resize-none outline-none focus:bg-transparent focus:border-b-2 text-xl focus:border-[#B8336A] border-b-2 border-transparent'  value= {about}/> */}
                     <textarea  disabled={disabledTextarea}  ref={aboutInput}  id="about" rows="3" className='focus:text-[#fff] focus:opacity-100 opacity-80 text-[#f1f1f1] mt-2 cursor-pointer ring-0 border-t-0 border-r-0 border-b-2 border-l-0 w-5/6 bg-transparent outline-offset-0 resize-none outline-none focus:bg-transparent focus:border-b-2 focus:ring-0  focus:outline-offset-0 text-xl focus:border-[#B8336A] border-transparent p-0' value={about}></textarea>
-                    <Tooltip content={"Edit"} style="light"  animation="duration-500">
-                        <img className='w-5 cursor-pointer  duration-300 hover:rotate-6 ' onClick={handleTextArea} src={edit} alt="edit" />
+                    <Tooltip content={disabledTextarea?"Edit":"Save"} style="light"  animation="duration-500">
+                        <img className='w-5 cursor-pointer  duration-300 hover:rotate-6 ' onClick={handleTextArea} src={disabledTextarea?edit:check} alt="edit" />
 
                     </Tooltip>
                 </div>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,6 +8,20 @@ module.exports = {
       height: {
         '128': '600px',
       },
+      keyframes: {
+        fadeOut: {
+          '0%': { opacity: 1 },
+          '100%': { opacity: 0 },
+        },
+        fadeIn: {
+          '0%': { opacity: 0 },
+          '100%': { opacity: 1},
+        }
+      },
+      animation: {
+        fadeOut: 'fadeOut 1s ease-in-out',
+        fadeIn: 'fadeIn 1s ease-in-out',
+      },
       maxHeight: {
         '128': '70%',
       },
