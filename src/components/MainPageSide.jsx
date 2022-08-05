739f9f2a4925161abba7acc7296b6b23fe660b3d
Elbis1 <64051166+Elbis1@users.noreply.github.com>
2022-08-05 23:54:20 +0300

    helper outsideClick function added

--- src/components/MainPageSide.jsx
+++ src/components/MainPageSide.jsx
@@ -4,24 +4,45 @@ import profileImage from "./../images/butters.png"
 import edit from "./../images/edit.svg"
 import copyIcon from "./../images/copy-1.svg"
 import { Avatar, Tooltip,Textarea } from 'flowbite-react';
+import useClickOutside from '../helper/useClickOutside ';
 
 export default function MainPageSide() {
     const [name,setName]=useState("Kağan Gencer")
     const [about,setAbout]=useState("Hey there, I am using... asdasdasd asdasdas asdasd")
     const [isCopied, setCopied] = React.useState(false);
-
+    const [disabledInput,setDisabledInput]=useState(false)
+    const [disabledTextarea,setDisabledTextarea]=useState(false)
 
     const nameInput = useRef(null);
     const aboutInput = useRef(null);
 
+    const clickRefInput = React.useRef();
+    const clickRefTextarea = React.useRef();
+
+    useClickOutside(clickRefInput,()=>{
+        setDisabledInput(true)
+    });
+
+    useClickOutside(clickRefTextarea,()=>{
+        setDisabledTextarea(true)
+    });
+
 
     function handleInputDisabled(){
-        nameInput.current.focus();
+        setDisabledInput(false)
+        setTimeout(()=>{
+            nameInput.current.focus();
+          },50)
     }
 
 
+
+
     function handleTextArea(){
-        aboutInput.current.focus();
+        setDisabledTextarea(false)
+        setTimeout(()=>{
+            aboutInput.current.focus();
+        },50)
     }
 
     function copy(){
@@ -46,18 +67,18 @@ export default function MainPageSide() {
             </div>
             <div className='  w-5/6 pr-2 pl-2 justify-between'>
                 <span className='text-[#B8336A] text-xl font-semibold cursor-default'>Your name</span>
-                <div className='flex justify-between items-center'>
-                    <input ref={nameInput}   className='text-[#f1f1f1] focus:opacity-100 opacity-80 cursor-pointer w-5/6 bg-transparent outline-offset-0  outline-none focus:bg-transparent font-normal focus:text-[#fff] text-xl focus:border-b-2 mt-2 border-b-2 border-transparent focus:border-[#B8336A] ' value= {name}></input>
-                    <Tooltip content={"Edit"} style="light"  animation="duration-500">
-                        <img className='w-5 cursor-pointer mb-1 duration-300 hover:rotate-6 ' onClick={handleInputDisabled} src={edit} alt="edit" />
+                <div  ref={clickRefInput}  className='flex justify-between items-center'>
+                    <input   ref={nameInput}  disabled={disabledInput}  className='text-[#f1f1f1] focus:opacity-100 opacity-80 cursor-pointer w-5/6 bg-transparent outline-offset-0  outline-none focus:bg-transparent font-normal focus:text-[#fff] text-xl focus:border-b-2 mt-2 border-b-2 border-transparent focus:border-[#B8336A] ' value= {name}></input>
+                    <Tooltip  content={"Edit"} style="light"  animation="duration-500">
+                        <img   className='w-5 cursor-pointer mb-1 duration-300 hover:rotate-6 ' onClick={handleInputDisabled} src={edit} alt="edit" />
                     </Tooltip>
                 </div>
             </div>
             <div className=' mt-4 w-5/6 pr-2 pl-2 justify-between  '>
                 <span className='text-[#B8336A]  text-xl font-semibold cursor-default'>About</span>
-                <div className='flex justify-between items-center'>
+                <div ref={clickRefTextarea} className='flex justify-between items-center'>
 {/*                     <Textarea ref={aboutInput}  id="about"  rows={4}  className='focus:text-[#fff] focus:opacity-100 opacity-80 text-[#f1f1f1] mt-2 cursor-pointer w-5/6 bg-transparent outline-offset-0 resize-none outline-none focus:bg-transparent focus:border-b-2 text-xl focus:border-[#B8336A] border-b-2 border-transparent'  value= {about}/> */}
-                    <textarea  ref={aboutInput}  id="about" rows="3" className='focus:text-[#fff] focus:opacity-100 opacity-80 text-[#f1f1f1] mt-2 cursor-pointer ring-0 border-t-0 border-r-0 border-b-2 border-l-0 w-5/6 bg-transparent outline-offset-0 resize-none outline-none focus:bg-transparent focus:border-b-2 focus:ring-0  focus:outline-offset-0 text-xl focus:border-[#B8336A] border-transparent p-0' value={about}></textarea>
+                    <textarea  disabled={disabledTextarea}  ref={aboutInput}  id="about" rows="3" className='focus:text-[#fff] focus:opacity-100 opacity-80 text-[#f1f1f1] mt-2 cursor-pointer ring-0 border-t-0 border-r-0 border-b-2 border-l-0 w-5/6 bg-transparent outline-offset-0 resize-none outline-none focus:bg-transparent focus:border-b-2 focus:ring-0  focus:outline-offset-0 text-xl focus:border-[#B8336A] border-transparent p-0' value={about}></textarea>
                     <Tooltip content={"Edit"} style="light"  animation="duration-500">
                         <img className='w-5 cursor-pointer  duration-300 hover:rotate-6 ' onClick={handleTextArea} src={edit} alt="edit" />
 
