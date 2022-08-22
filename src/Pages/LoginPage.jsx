5bcf3ee981a60864679e22afab94ad3f73bb58d0
kaganBerk1 <kaan_berk_gencer@hotmail.com>
2022-08-22 21:57:09 +0300

    login page tablet form done

--- src/Pages/LoginPage.jsx
+++ src/Pages/LoginPage.jsx
@@ -41,8 +41,8 @@ export default function LoginPage() {
 
   return (
     <div  className=' bg-gradient-to-t from-[#8abdd8] via-purple-500 to-[#bfe9ff] flex justify-center items-center min-h-screen'>
-        <div  className='shadow-2xl xl:rounded-3xl xl:aspect-video xl:w-3/4  w-full  h-screen  bg-[#313131] flex flex-col items-center justify-around'>
-        <section style={{border:"0.1px solid #f1f1f1",borderRadius:"25px"}} className=" py-12  xl:py-12  xl:w-2/6 xl:h-4/6 xl:px-8 ">
+        <div  className='shadow-2xl xl:rounded-3xl xl:aspect-video xl:w-3/4  w-full  h-screen xl:h-auto  bg-[#313131] flex flex-col items-center justify-around'>
+        <section style={{border:"0.1px solid #f1f1f1",borderRadius:"25px"}} className=" md:px-20 md:py-48 py-12  xl:py-12  xl:w-2/6 xl:h-4/6 xl:px-8 ">
             <div className="px-6 h-full text-gray-800">
                 <div
                 className="flex xl:justify-center lg:justify-between justify-center items-center flex-wrap h-full g-6"
@@ -55,7 +55,7 @@ export default function LoginPage() {
                     <div className="mb-6">
                         <input
                         type="text"
-                        className="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                        className="form-control block w-full px-4 py-2 xl:py-2  md:py-6 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
                         id="exampleFormControlInput2"
                         placeholder="Mail"
                         onChange={(e)=>handleMail(e.target.value)}
@@ -66,7 +66,7 @@ export default function LoginPage() {
                     <div className="mb-6">
                         <input
                         type="password"
-                        className="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                        className="form-control block w-full px-4 xl:py-2  md:py-6 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
                         id="exampleFormControlInput2"
                         placeholder="Password"
                         onChange={(e)=>handlePassword(e.target.value)}
@@ -80,12 +80,12 @@ export default function LoginPage() {
                     <div className="text-center lg:text-left w-full mb-8">
                         <button
                         type="button"
-                        className="inline-block px-7 w-full py-3  bg-blue-600 text-[#f1f1f1] font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-[#a185e7] hover:shadow-lg focus:bg-[#a185e7] focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
+                        className="inline-block xl:text-sm px-7 md:text-lg w-full py-3 xl:py-3 md:py-6  bg-blue-600 text-[#f1f1f1] font-medium text-sm leading-snug uppercase rounded shadow-md hover:bg-[#a185e7] hover:shadow-lg focus:bg-[#a185e7] focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
                         onClick={login}
                         >
                         Login
                         </button>
-                        <p className="text-sm text-[#f1f1f1] font-semibold mt-2 pt-1 mb-0">
+                        <p className="text-sm xl:text-sm md:text-lg text-[#f1f1f1] font-semibold mt-2 pt-1 mb-0">
                         Don't have an account?
                         <a
                             href="/register"
